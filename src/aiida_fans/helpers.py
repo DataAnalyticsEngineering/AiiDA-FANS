bd72aa8e918f11240f4c49ee2dee2a6b5033eb58
Ethan Shanahan <ethan.shanahan@gmail.com>
2025-05-26 16:50:09 +0200

    def input generator tool

--- src/aiida_fans/helpers.py
+++ src/aiida_fans/helpers.py
@@ -1,30 +1,37 @@
-"""Tools required by aiida_fans."""
+"""Tools required by aiida-fans."""
 
-import json
+from typing import Any
 
-from aiida.orm import ArrayData, Dict, Float, Int, List, SinglefileData, Str
+from aiida.engine import CalcJob
 from numpy import allclose, ndarray
 
 
-class InputEncoder(json.JSONEncoder):
-    """Prepares a dictionary of calcjob inputs for json representation."""
-
-    def default(self, obj):
-        """Converts aiida datatypes to their python counterparts."""
-        match obj:
-            case Str() | Int() | Float():
-                return obj.value
-            case List():
-                return obj.get_list()
-            case Dict():
-                return obj.get_dict()
-            case ArrayData():
-                return [a[1].tolist() for a in obj.get_iterarrays()]  #! Caution: may be disordered
-            case SinglefileData():
-                return obj.filename
-            case _:
-                # Let the base class default method raise the TypeError
-                return super().default(obj)
+def make_input_dict(job: CalcJob) -> dict[str, Any]:
+    """Prepares a dictionary that maps to an input.json from calcjob inputs."""
+    return {
+        ## Microstructure Definition
+        "microstructure": {
+            "filename": None, # path to stashed microstructure, must be overwritten by impl
+            "datasetname": job.inputs.microstructure.datasetname.value,
+            "L": job.inputs.microstructure.L.get_list()
+        },
+        ## Problem Type and Material Model
+        "problem_type": job.inputs.problem_type.value,
+        "matmodel": job.inputs.matmodel.value,
+        "material_properties": job.inputs.material_properties.get_dict(),
+        ## Solver Settings
+        "method": job.inputs.method.value,
+        "n_it": job.inputs.n_it.value,
+        "error_parameters": {
+            "measure": job.inputs.error_parameters.measure.value,
+            "type": job.inputs.error_parameters.type.value,
+            "tolerance": job.inputs.error_parameters.tolerance.value
+        },
+        ## Macroscale Loading Conditions
+        "macroscale_loading": job.inputs.macroscale_loading.get_list(),
+        ## Results Specification
+        "results": job.inputs.results.get_list()
+    }
 
 def arraydata_equal(first: dict[str, ndarray], second: dict[str, ndarray]) -> bool:
     """Return whether two dicts of arrays are roughly equal."""
